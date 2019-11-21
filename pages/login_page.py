c41cf06da7a4a1b1316a4cb82bdbda95470e438e
olga_urazlova <olga_urazlova@epam.com>
2019-11-21 16:29:13 +0300

    change browser.implicitly_wait a

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,10 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.browser.current_url == "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/", "На данной странице нет функционала логина и регистрации"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "There's no login form"
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "There's no register form"
