53a528e086454d003f788dc20c2217ba7c7e920c
olga_urazlova <olga_urazlova@epam.com>
2019-11-21 16:52:52 +0300

    Main_page - click login link - accept alert

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
 from .login_page import LoginPage
-from selenium.webdriver.common.by import By
 
 class MainPage(BasePage):
     def go_to_login_page(self):
         link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
+        alert = self.browser.switch_to_alert
+        alert.accept()
         #return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
